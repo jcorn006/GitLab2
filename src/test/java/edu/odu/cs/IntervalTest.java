2cfb2aaf3f8f5601315a0b2f029e4379454f5882
jcorn006 <jcorn006@odu.edu>
2022-07-23 15:47:13 -0400

    added tests

---
+++ src/test/java/edu/odu/cs/IntervalTest.java
@@ -0,0 +1,20 @@
+package edu.odu.cs;
+
+import org.junit.jupiter.api.Test;
+
+public class IntervalTest {
+    @Test
+    void testEmpty() {
+
+    }
+
+    @Test
+    void testGetMax() {
+
+    }
+
+    @Test
+    void testGetMin() {
+
+    }
+}
